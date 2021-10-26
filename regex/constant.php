0c93ba9a5528740bbd9e153042bd588290ba3d66
nguyenthixuananh <nguyenhixuananh09061999@gmail.com>
2021-10-26 16:23:23 +0700

    regex-register-login

---
+++ regex/constant.php
@@ -0,0 +1,7 @@
+<?php
+define("EMAIL_REQUIRE", "Email must not be empty");
+define("EMAIL_INVALID", "Email is not a valid email address");
+define("PASSWORD_REQUIRE", "Password must not be empty");
+define("PASSWORD_INVALID", "Password super weak");
+define("PASSWORD_REPEAT_REQUIRE", "Password confirmation must not be empty");
+define("PASSWORD_REPEAT_NOMATCH", "Password confirmation must match");
